64fe223e25816621ea7620f99e78a8ce17b9ea52
Yusuf R <notYusufR@gmail.com>
2023-01-28 23:02:12 -0500

    More routing stuff

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,17 +10,11 @@ router.post('/', (req, res) => {
     res.json({ message: "Create new user" });
 });
 
-<<<<<<< HEAD
-router.put('/', (req, res) => {
-    res.status(200).json({ message: "Update users" });
-=======
-
-router.put('/uid', (req, res) => {
+router.put('/:uid', (req, res) => {
     res.status(200).json({ message: 'Update users ${req.params._id}' })
->>>>>>> f720299 (Hulk is quite beautiful)
 });
 
-router.delete('/uid', (req, res) => {
+router.delete('/:uid', (req, res) => {
     res.status(200).json({ message: "Delete user ${req.params._id" })
 });
 
